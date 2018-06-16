2449ce4e9f6894cbe8673b4c97857b246a4bc3c1
Ashish Chouhan <ashish.s.chouhan@gmail.com>
2018-06-16 17:32:19 +0200

    Duplicate SOurces were present in master

--- README.md
+++
@@ -1,2 +0,0 @@
-# srh-sdp-2018-job-portal-system
-Design a common platform using which a user can access information of job opening from different job boards.
--- start_db.php
+++
@@ -1,5 +0,0 @@
-<?php
-$con=mysqli_connect('localhost','Ashish','A123456789') or die('Connection refused to MySQL');
-mysqli_select_db($con,'final_exam_demo') or die('Connection refused to database');
-//echo 'Connected';
-?>
